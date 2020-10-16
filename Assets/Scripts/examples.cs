098dd149a8ea74fdf0ef7c10d99e8c239e82ec4b
hellburn66 <sfeaste1@email.cpcc.edu>
2020-10-16 14:16:17 -0400

    Add new items

---
+++ Assets/Scripts/examples.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class examples : MonoBehaviour
+{   public enum Ename
+    {
+        MrDoom,
+        GoldenMatch,
+        Mayor
+       
+
+    }
+    public Ename tile= Ename.MrDoom;
+
+    public string display_tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (tile)
+        {
+            case Ename.Mayor:Debug.Log("I'm the Mayor"); break;
+            case Ename.MrDoom: Debug.Log("I'm the Villan"); break;
+            case Ename.GoldenMatch: Debug.Log("I'm the Hero"); break;
+            default: Debug.Log("I'm the something"); break;
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
