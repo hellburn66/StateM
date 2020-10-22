ccf74ac281bb188911c6dc00fd19f43796d66858
hellburn66 <sfeaste1@email.cpcc.edu>
2020-10-22 12:22:55 -0400

    new items

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum State
+    {idle,
+     walking,
+     swimming,
+     climbing
+      
     }
 
-    // Update is called once per frame
+    public State mainstate = State.idle;
+    Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+
+
+    }
+
     void Update()
     {
-        
+        switch (mainstate)
+        {
+            case State.idle:
+                Idle();
+                break;
+                
+            case State.walking:Walking(); break;
+            case State.swimming:Swimming(); break;
+            case State.climbing:Climbing(); break;
+            default:break;
+
+        }
     }
+
+     void OnTriggerEnter(Collider other)
+     {
+        if (other.name == "WaterTrigger")
+        {
+            mainstate = State.swimming; 
+        }
+        else if (other.name== "MountainTrigger")
+        {
+            mainstate = State.climbing;
+        }
+     }
+     void OnTriggerExit(Collider other)
+    {
+        mainstate = State.walking;
+    }
+    void Swimming()
+    {
+        Debug.Log("I'm Swimming");
+    }
+    void Climbing()
+    {
+        Debug.Log("I'm Climbing");
+    }
+    void Idle()
+    {
+        Debug.Log("Im idle");
+        if(lastPosition != transform.position)
+        {
+            mainstate = State.walking;
+        }
+        lastPosition = transform.position;
+
+    }
+    void Walking()
+    {
+        Debug.Log("I'm walking");
+        if(lastPosition== transform.position)
+        {
+            mainstate = State.idle;
+        }
+        lastPosition = transform.position;
+    }
+
+
+
 }
